9b73f671a5364f8cb02ef4701d4f19072156e38f
TriHuynh <tri.huynh@gmail.com>
2021-06-17 13:23:11 +0700

    Refs [Task] [#37314] Cài đặt i18n

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'react-toastify/dist/ReactToastify.css';
+import 'react-multi-carousel/lib/styles.css';
+import './styles/styles.scss';
+import configureStore from './redux/configureStore';
+import {Provider} from 'react-redux';
+import {I18nextProvider} from 'react-i18next';
+import i18next from './i18next';
+
+const store = configureStore();
+
+ReactDOM.render(
+  <I18nextProvider i18n={i18next}>
+    <Provider store={store}>
+      <React.StrictMode>
+          <App />
+      </React.StrictMode>
+    </Provider>
+  </I18nextProvider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
